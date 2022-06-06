5869b5394c20873c0a49a52cb78588ff05714523
ckdals82 <81973067+ckdals82@users.noreply.github.com>
2022-06-06 21:27:07 +0900

    no message

---
+++ 1-1-basic.ts
@@ -0,0 +1,21 @@
+{
+  //javaScript
+  //number
+  const num: number = 3;
+
+  //string
+  const str: string = '안녕 타입스크립트';
+
+  //boolean
+  const boal: boolean = false;
+
+  //undefined
+  let age1: undefined; //💩
+  let age: number | undefined;
+  age = undefined;
+  age = 1;
+
+  //null
+  let person: null; //💩
+  person = null;
+}
