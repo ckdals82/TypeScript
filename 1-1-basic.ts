ae253c32cc0cc784ad2c6cdd6e432f6e3c0c1fe1
ckdals82 <81973067+ckdals82@users.noreply.github.com>
2022-06-07 22:39:50 +0900

    no message

--- 1-1-basic.ts
+++ 1-1-basic.ts
@@ -18,4 +18,21 @@
   //null
   let person: null; //💩
   person = null;
+
+  //unknown //💩
+  let notSure: unknown = 0;
+  //어떤 타입이든 들어오는것 .
+  //가능하면 웬만하면 쓰지 않는 것이 좋음.
+
+  //any 💩
+  //이 친구도 가능하면 쓰지 않는 것이 좋음
+
+  //void
+  //함수에서 return 하는 것이 아무것도 없을때 , 일반 변수에서 할당하면 undefined밖에 못 들어감!
+  function vo(): void {
+    return;
+  }
+
+  //never
+  //리턴문이 없다.
 }
